7c148e8e5c96016d8cd6411edba0283901d753e1
gudni <gudnihrafn93@gmail.com>
2025-07-29 21:27:07 +0000

    cartridge baby

---
+++ cartridge/cartirdge.cpp
@@ -0,0 +1,377 @@
+#include <cstdint>
+#include <cstdio>
+#include <iostream>
+#include <unordered_map>
+#include <string>
+
+std::unordered_map<std::string, std::string> new_licensee_codes = {
+    {"00", "None"},
+    {"01", "Nintendo Research & Development 1"},
+    {"08", "Capcom"},
+    {"13", "EA (Electronic Arts)"},
+    {"18", "Hudson Soft"},
+    {"19", "b-Att"},
+    {"20", "KSS"},
+    {"22", "Planning Office WADA"},
+    {"24", "PCM Complete"},
+    {"25", "San-X"},
+    {"28", "Kemco"},
+    {"29", "SETA Corporation"},
+    {"30", "Viacom"},
+    {"31", "Nintendo"},
+    {"32", "Bandai"},
+    {"33", "Ocean Software/Acclaim Entertainment"},
+    {"34", "Konami"},
+    {"35", "HectorSoft"},
+    {"37", "Taito"},
+    {"38", "Hudson Soft"},
+    {"39", "Banpresto"},
+    {"41", "Ubi Soft"},
+    {"42", "Atlus"},
+    {"44", "Malibu Interactive"},
+    {"46", "Angel"},
+    {"47", "Bullet-Proof Software"},
+    {"49", "Irem"},
+    {"50", "Absolute"},
+    {"51", "Acclaim Entertainment"},
+    {"52", "Activision"},
+    {"53", "Sammy USA Corporation"},
+    {"54", "Konami"},
+    {"55", "Hi Tech Expressions"},
+    {"56", "LJN"},
+    {"57", "Matchbox"},
+    {"58", "Mattel"},
+    {"59", "Milton Bradley Company"},
+    {"60", "Titus Interactive"},
+    {"61", "Virgin Games Ltd."},
+    {"64", "Lucasfilm Games"},
+    {"67", "Ocean Software"},
+    {"69", "EA (Electronic Arts)"},
+    {"70", "Infogrames"},
+    {"71", "Interplay Entertainment"},
+    {"72", "Broderbund"},
+    {"73", "Sculptured Software"},
+    {"75", "The Sales Curve Limited"},
+    {"78", "T*HQ"},
+    {"79", "Accolade"},
+    {"80", "Misawa Entertainment"},
+    {"82", "Toze"},
+    {"83", "Tokuma Shoten"},
+    {"84", "Tsukuda Original"},
+    {"86", "Chunsoft Co."},
+    {"87", "Video System"},
+    {"91", "Ocean Software/Acclaim Entertainment"},
+    {"92", "Varie"},
+    {"93", "Yonezawa/s’pal"},
+    {"95", "Kaneko"},
+    {"96", "Pack-In-Video"},
+    {"97", "Bottom Up"},
+    {"98", "Konami (Yu-Gi-Oh!)"},
+    {"99", "MTO"},
+    {"A4", "Kodansha"}
+};
+
+
+std::unordered_map<uint8_t, std::string> old_licensee_codes = {
+    {0x00, "None"},
+    {0x01, "Nintendo"},
+    {0x08, "Capcom"},
+    {0x09, "HOT-B"},
+    {0x0A, "Jaleco"},
+    {0x0B, "Coconuts Japan"},
+    {0x0C, "Elite Systems"},
+    {0x13, "EA (Electronic Arts)"},
+    {0x18, "Hudson Soft"},
+    {0x19, "ITC Entertainment"},
+    {0x1A, "Yanoman"},
+    {0x1D, "Japan Clary"},
+    {0x1F, "Virgin Games Ltd."},
+    {0x24, "PCM Complete"},
+    {0x25, "San-X"},
+    {0x28, "Kemco"},
+    {0x29, "SETA Corporation"},
+    {0x30, "Infogrames"},
+    {0x31, "Nintendo"},
+    {0x32, "Bandai"},
+    {0x33, "Use new licensee code"},
+    {0x34, "Konami"},
+    {0x35, "HectorSoft"},
+    {0x38, "Capcom"},
+    {0x39, "Banpresto"},
+    {0x3C, "Entertainment Interactive (stub)"},
+    {0x3E, "Gremlin"},
+    {0x41, "Ubi Soft"},
+    {0x42, "Atlus"},
+    {0x44, "Malibu Interactive"},
+    {0x46, "Angel"},
+    {0x47, "Spectrum HoloByte"},
+    {0x49, "Irem"},
+    {0x4A, "Virgin Games Ltd."},
+    {0x4D, "Malibu Interactive"},
+    {0x4F, "U.S. Gold"},
+    {0x50, "Absolute"},
+    {0x51, "Acclaim Entertainment"},
+    {0x52, "Activision"},
+    {0x53, "Sammy USA Corporation"},
+    {0x54, "GameTek"},
+    {0x55, "Park Place"},
+    {0x56, "LJN"},
+    {0x57, "Matchbox"},
+    {0x59, "Milton Bradley Company"},
+    {0x5A, "Mindscape"},
+    {0x5B, "Romstar"},
+    {0x5C, "Naxat Soft"},
+    {0x5D, "Tradewest"},
+    {0x60, "Titus Interactive"},
+    {0x61, "Virgin Games Ltd."},
+    {0x67, "Ocean Software"},
+    {0x69, "EA (Electronic Arts)"},
+    {0x6E, "Elite Systems"},
+    {0x6F, "Electro Brain"},
+    {0x70, "Infogrames"},
+    {0x71, "Interplay Entertainment"},
+    {0x72, "Broderbund"},
+    {0x73, "Sculptured Software"},
+    {0x75, "The Sales Curve Limited"},
+    {0x78, "THQ"},
+    {0x79, "Accolade"},
+    {0x7A, "Triffix Entertainment"},
+    {0x7C, "MicroProse"},
+    {0x7F, "Kemco"},
+    {0x80, "Misawa Entertainment"},
+    {0x83, "LOZC G."},
+    {0x86, "Tokuma Shoten"},
+    {0x8B, "Bullet-Proof Software"},
+    {0x8C, "Vic Tokai Corp."},
+    {0x8E, "Ape Inc."},
+    {0x8F, "I’Max"},
+    {0x91, "Chunsoft Co."},
+    {0x92, "Video System"},
+    {0x93, "Tsubaraya Productions"},
+    {0x95, "Varie"},
+    {0x96, "Yonezawa/S’Pal"},
+    {0x97, "Kemco"},
+    {0x99, "Arc"},
+    {0x9A, "Nihon Bussan"},
+    {0x9B, "Tecmo"},
+    {0x9C, "Imagineer"},
+    {0x9D, "Banpresto"},
+    {0x9F, "Nova"},
+    {0xA1, "Hori Electric"},
+    {0xA2, "Bandai"},
+    {0xA4, "Konami"},
+    {0xA6, "Kawada"},
+    {0xA7, "Takara"},
+    {0xA9, "Technos Japan"},
+    {0xAA, "Broderbund"},
+    {0xAC, "Toei Animation"},
+    {0xAD, "Toho"},
+    {0xAF, "Namco"},
+    {0xB0, "Acclaim Entertainment"},
+    {0xB1, "ASCII Corporation / Nexsoft"},
+    {0xB2, "Bandai"},
+    {0xB4, "Square Enix"},
+    {0xB6, "HAL Laboratory"},
+    {0xB7, "SNK"},
+    {0xB9, "Pony Canyon"},
+    {0xBA, "Culture Brain"},
+    {0xBB, "Sunsoft"},
+    {0xBD, "Sony Imagesoft"},
+    {0xBF, "Sammy Corporation"},
+    {0xC0, "Taito"},
+    {0xC2, "Kemco"},
+    {0xC3, "Square"},
+    {0xC4, "Tokuma Shoten"},
+    {0xC5, "Data East"},
+    {0xC6, "Tonkin House"},
+    {0xC8, "Koei"},
+    {0xC9, "UFL"},
+    {0xCA, "Ultra Games"},
+    {0xCB, "VAP, Inc."},
+    {0xCC, "Use Corporation"},
+    {0xCD, "Meldac"},
+    {0xCE, "Pony Canyon"},
+    {0xCF, "Angel"},
+    {0xD0, "Taito"},
+    {0xD1, "SOFEL (Software Engineering Lab)"},
+    {0xD2, "Quest"},
+    {0xD3, "Sigma Enterprises"},
+    {0xD4, "ASK Kodansha Co."},
+    {0xD6, "Naxat Soft"},
+    {0xD7, "Copya System"},
+    {0xD9, "Banpresto"},
+    {0xDA, "Tomy"},
+    {0xDB, "LJN"},
+    {0xDD, "Nippon Computer Systems"},
+    {0xDE, "Human Entertainment"},
+    {0xDF, "Altron"},
+    {0xE0, "Jaleco"},
+    {0xE1, "Towa Chiki"},
+    {0xE2, "Yutaka"},
+    {0xE3, "Varie"},
+    {0xE5, "Epoch"},
+    {0xE7, "Athena"},
+    {0xE8, "Asmik Ace Entertainment"},
+    {0xE9, "Natsume"},
+    {0xEA, "King Records"},
+    {0xEB, "Atlus"},
+    {0xEC, "Epic/Sony Records"},
+    {0xEE, "IGS"},
+    {0xF0, "A Wave"},
+    {0xF3, "Extreme Entertainment"},
+    {0xFF, "LJN"}
+};
+
+std::unordered_map<uint8_t, std::string> cartridge_type_map = {
+    {0x00, "ROM ONLY"},
+    {0x01, "MBC1"},
+    {0x02, "MBC1+RAM"},
+    {0x03, "MBC1+RAM+BATTERY"},
+    {0x05, "MBC2"},
+    {0x06, "MBC2+BATTERY"},
+    {0x08, "ROM+RAM"},
+    {0x09, "ROM+RAM+BATTERY"},
+    {0x0B, "MMM01"},
+    {0x0C, "MMM01+RAM"},
+    {0x0D, "MMM01+RAM+BATTERY"},
+    {0x0F, "MBC3+TIMER+BATTERY"},
+    {0x10, "MBC3+TIMER+RAM+BATTERY"},
+    {0x11, "MBC3"},
+    {0x12, "MBC3+RAM"},
+    {0x13, "MBC3+RAM+BATTERY"},
+    {0x19, "MBC5"},
+    {0x1A, "MBC5+RAM"},
+    {0x1B, "MBC5+RAM+BATTERY"},
+    {0x1C, "MBC5+RUMBLE"},
+    {0x1D, "MBC5+RUMBLE+RAM"},
+    {0x1E, "MBC5+RUMBLE+RAM+BATTERY"},
+    {0x20, "MBC6"},
+    {0x22, "MBC7+SENSOR+RUMBLE+RAM+BATTERY"},
+    {0xFC, "POCKET CAMERA"},
+    {0xFD, "BANDAI TAMA5"},
+    {0xFE, "HuC3"},
+    {0xFF, "HuC1+RAM+BATTERY"}
+};
+
+std::unordered_map<uint8_t, char> ROM_banks_map = {
+    {0x00, 2},    // 32 KiB
+    {0x01, 4},    // 64 KiB
+    {0x02, 8},    // 128 KiB
+    {0x03, 16},   // 256 KiB
+    {0x04, 32},   // 512 KiB
+    {0x05, 64},   // 1 MiB
+    {0x06, 128},  // 2 MiB
+    {0x07, 256},  // 4 MiB
+    {0x08, 512},  // 8 MiB
+    {0x52, 72},   // 1.1 MiB
+    {0x53, 80},   // 1.2 MiB
+    {0x54, 96}    // 1.5 MiB
+};
+
+std::unordered_map<uint8_t, char> RAM_size_map = {
+    {0x00, 0},      // No RAM
+    {0x01, -1},     // Unused
+    {0x02, 8},      // 1 bank
+    {0x03, 32},     // 4 banks of 8 KiB each
+    {0x04, 128},    // 16 banks of 8 KiB each
+    {0x05, 64}      // 8 banks of 8 KiB each
+};
+
+#define TITLE_LENGTH    0xF
+#define NINTENDO_LOGO   0x0104   // to 0x0133
+#define TITLE           0x0134   // to 0x0143
+#define CGB_FLAG        0x0143
+#define SGB_FLAG        0x0146
+#define CART_TYPE       0x0147
+#define ROM_BANKS       0x0148
+#define RAM_SIZE        0x0149
+#define DESTINATION     0X014A
+#define OLD_LICENSEE    0x014B
+#define NEW_LICENSEE    0x0144   // to 0x145
+#define ROM_VERSION     0x014C
+
+class Cartridge {
+public:
+    const char *filename;
+    uint8_t *ROM = nullptr;
+    int ROM_size;
+    std::string title;
+    std::string developer;
+    std::string cartridge_type;
+    std::string destination;
+    uint8_t CGB_flag;
+    uint8_t SGB_flag;
+    uint8_t ROM_banks;
+    uint8_t RAM_size;
+
+    Cartridge(const char *_filename){
+        filename = _filename;
+        load_cartridge();
+        
+        for (int i = 0; i < TITLE_LENGTH; i++)
+            title += ROM[TITLE + i];
+
+        if (ROM[OLD_LICENSEE] == 0x33) {
+            char ascii_lo = ROM[NEW_LICENSEE]; 
+            char ascii_hi = ROM[NEW_LICENSEE + 1];
+            std::string code;
+            code += ascii_lo;
+            code += ascii_hi;
+            developer = new_licensee_codes[code];
+        } else {
+            developer = old_licensee_codes[ROM[OLD_LICENSEE]];
+        }
+
+        SGB_flag = ROM[SGB_FLAG];
+        CGB_flag = ROM[CGB_FLAG];
+        ROM_banks = ROM_banks_map[ROM[ROM_BANKS]];
+        RAM_size = RAM_size_map[ROM[RAM_SIZE]];
+        cartridge_type = cartridge_type_map[ROM[CART_TYPE]];
+    }
+
+
+    bool load_cartridge() {
+        FILE *fp = fopen(filename, "rb");
+
+        if (!fp) {
+            std::cout << "Failed to open " <<  filename << "\n";
+            return false;
+        }
+
+        std::cout << "Opened: " << filename << "\n";
+
+        fseek(fp, 0, SEEK_END);
+        ROM_size = ftell(fp);
+        rewind(fp);
+        ROM = (uint8_t*)malloc(ROM_size);
+        fread(ROM, 1, ROM_size, fp);
+        fclose(fp);
+        
+        return true;
+    }
+
+    uint8_t get_byte (uint16_t i) {
+        return ROM[i];
+    }
+
+    void print_info() {
+        std::cout << "Title: " << title << "\n";
+        std::cout << "Developer: " << developer << "\n";
+        std::cout << "ROM Size: " << ROM_size << "\n";
+        std::cout << "Cartridge Type: " << cartridge_type << "\n";
+    }
+
+    void close_cartridge() {
+        free(ROM);
+    }
+};
+
+
+int main() {
+    const char *filename = "../Tetris_(USA)_(Rev-A).gb";
+    Cartridge cart = Cartridge(filename);
+    cart.print_info();
+    cart.close_cartridge();
+    
+    return 0;
+}
